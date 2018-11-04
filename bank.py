7b3365e16e709a50e84db13030881e9248cd5338
Mehul Bhuva <mbhuva1@cs.binghamton.edu>
2018-11-04 00:21:01 -0400

    implementaion of Init_branch Message

--- bank.py
+++ bank.py
@@ -10,12 +10,25 @@ logger = logging.getLogger(__name__)
 sys.path.append('/home/vchaska1/protobuf/protobuf-3.5.1/python')
 import bank_pb2
 
-if __name__ == '__main__':
 
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind((socket.gethostbyname(socket.gethostname()), int(sys.argv[2])))
-    serversocket.listen(5)
-    logger.debug("\nWaiting for connection... Listening on "+str(socket.gethostbyname(socket.gethostname())+":"+ sys.argv[2]))
-    while 1:
-        (clientsocket, address) = serversocket.accept()
-	data = pickle.loads(clientsocket.recv(1024))
+
+branchName = ""
+branchBalance = 0
+branchLst = []
+
+if __name__ == '__main__':
+	branchName = sys.argv[1]
+	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	serversocket.bind((socket.gethostbyname(socket.gethostname()), int(sys.argv[2])))
+	serversocket.listen(5)
+	logger.debug("\nWaiting for connection... Listening on "+str(socket.gethostbyname(socket.gethostname())+":"+ sys.argv[2]))
+	while 1:
+        	(clientsocket, address) = serversocket.accept()
+		data = pickle.loads(clientsocket.recv(1024))
+		if data.HasField("init_branch") :
+			branchBalance = data.init_branch.balance
+			for branch in data.init_branch.all_branches:
+                        	if branch.name != branchName:
+                        		branchLst.append(branch)
+
+                print branchLst
--- controller.py
+++ controller.py
@@ -8,6 +8,7 @@ logger = logging.getLogger(__name__)
 sys.path.append('/home/vchaska1/protobuf/protobuf-3.5.1/python')
 import bank_pb2
 branhList = []
+initcount = 0
 
 if __name__ == '__main__':
 	
@@ -22,21 +23,23 @@ if __name__ == '__main__':
 
 	#print branhList
 	while True:
-        	divide_Balance = int(sys.argv[1]) / len(branhList)
-                for branch in branhList:
-                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    logger.debug("Initializing Branch:" + str(branch["name"]))
-                    s.connect((branch['ip'], branch['port']))
-                    initmessage = bank_pb2.InitBranch()
-                    initmessage.balance = divide_Balance                     
-                    for branchobj in branhList:
-                        branches = initmessage.all_branches.add()
-                        branches.name = branchobj['name']
-                        branches.ip = branchobj['ip']
-                        branches.port = branchobj['port']
+		if initcount == 0:
+        		divide_Balance = int(sys.argv[1]) / len(branhList)
+                	for branch in branhList:
+           	        	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                		logger.debug("Initializing Branch:" + str(branch["name"]))
+                    		s.connect((branch['ip'], branch['port']))
+                    		init_branch = bank_pb2.InitBranch()
+                    		init_branch.balance = divide_Balance                     
+                    		for branchobj in branhList:
+                        		branches = init_branch.all_branches.add()
+                        		branches.name = branchobj['name']
+                        		branches.ip = branchobj['ip']
+                        		branches.port = branchobj['port']
 
-                    message = bank_pb2.BranchMessage()
-                    message.init_branch.CopyFrom(initmessage)
+                    		message = bank_pb2.BranchMessage()
+                    		message.init_branch.CopyFrom(init_branch)
 
-                    s.sendall(pickle.dumps(message))
-                    s.close()
+                    		s.sendall(pickle.dumps(message))
+                    		s.close()
+			initcount = 1
